330508aede64a69ab8da33ccc3729f650d117f93
Lmahdi <ines.lmahdi@gmail.com>
2023-01-26 15:09:41 +0100

    Test connection

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,119 @@
 const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api/sauces', (req, res, next) => {
+  const stuff = [
+    {
+      _id:'sauce 1',
+      name:'sauce 1',
+      manufacturer:'sauce',
+      description:'sauce',
+      heat:'7',
+      likes:'',
+      dislikes:'',
+      imageUrl:'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      mainPepper:'sauce',
+      usersLiked:'',
+      usersDisliked:'',
+      userId:'sauce',
+    },
+    {
+      _id:'sauce 2',
+      name:'sauce 2',
+      manufacturer:'sauce',
+      description:'sauce',
+      heat:'9',
+      likes:'',
+      dislikes:'',
+      imageUrl:'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      mainPepper:'sauce',
+      usersLiked:'',
+      usersDisliked:'',
+      userId:'sauce',
+    },
+    {
+      _id:'sauce 4',
+      name:'sauce 1',
+      manufacturer:'sauce',
+      description:'sauce',
+      heat:'7',
+      likes:'',
+      dislikes:'',
+      imageUrl:'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      mainPepper:'sauce',
+      usersLiked:'',
+      usersDisliked:'',
+      userId:'sauce',
+    },
+    {
+      _id:'sauce 3',
+      name:'sauce 3',
+      manufacturer:'sauce',
+      description:'sauce',
+      heat:'5',
+      likes:'',
+      dislikes:'',
+      imageUrl:'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      mainPepper:'sauce',
+      usersLiked:'',
+      usersDisliked:'',
+      userId:'sauce',
+    },
+  ];
+  res.status(200).json(stuff);
+});
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const stuffRoutes = requiere('./routes/stuff');
+const path = require('path');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+const Thing = require('./models/Thing')
 
-const userRoutes = require('./routes/user.js');
-
-mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://ineslmahdi:Feeling1532@cluster0.wxafoym.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const app = express();
 
+//const app = express();
+/*
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
-});
+});*/
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+app.use('/api/sauces', stuffRoutes);
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
-app.use('/api/auth',userRoutes);
-
-module.exports = app;
+app.use('/api/auth', userRoutes);
 
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+
+// faire des console log pour voir ou est le probleme
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -29,7 +31,11 @@ exports.login = (req, res, next) => {
                 } else {
                     res.status(200).json({
                         userId: user._id,
-                        token: 'TOKEN'
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h'}
+                        )
                     });
                 }
             })
